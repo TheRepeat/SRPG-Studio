f2a563319bc01d01f9fec5625d27d00a785b72a0
TheRepeat <35505845+TheRepeat@users.noreply.github.com>
2020-05-16 22:07:34 -0500

    control-altering plugin

---
+++ alt-controls.js
@@ -0,0 +1,30 @@
+/* By Repeat.
+   Normally, button 2 (CANCEL) does 5 things. Five! 
+   Show enemy range, fast forward, fast cursor, unit menu, and cancel are all mapped to the same key. Insanity.
+   Me no likey, so this plugin separates those commands.
+   CANCEL (BTN2): show enemy range and cancel.
+   OPTION (BTN3): unit menu, as usual. What's unique is that it is now the only key that does this.
+   OPTION2 (BTN4): by default an unused key, now this handles fast forward and fast cursor.
+
+   InputControl.isAccelState is a new function. 
+   However, the other functions in this plugin override default functions without an alias.
+   Be sure this doesn't conflict with other plugins in your project.
+*/
+
+(function () {
+
+  // remap Accelerate (fast cursor/fast forward)
+  InputControl.isAccelState = function () {
+    return root.isInputState(InputType.BTN4);
+  }
+  MapCursor._isAccelerate = function () {
+    return InputControl.isAccelState();
+  }
+
+  // disable unit menu for Cancel
+  MapEdit._cancelAction = function (unit) {
+    if (!this._isMarkingDisabled && InputControl.getInputType() === InputType.NONE) {
+      MapLayer.getMarkingPanel().startMarkingPanel();
+    }
+  }
+})();

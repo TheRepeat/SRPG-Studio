52459114bbb16f899a15d38637179dfcee247800
TheRepeat <35505845+TheRepeat@users.noreply.github.com>
2020-06-09 23:45:56 -0500

    alt controls bugfix

--- alt-controls.js
+++ alt-controls.js
@@ -7,8 +7,17 @@
    SYSTEM (BTN7): handles fast forward like default, but now also handles fast cursor.
 
    InputControl.isAccelState is a new function. 
-   However, the other functions in this plugin override default functions without an alias.
+   SetupEdit._movPosChangeMode uses an alias, so it should be compatible with most other plugins.
+   However, the other functions in this plugin override vanilla functions without an alias.
    Be sure this doesn't conflict with other plugins in your project.
+   
+   WARNING: With this plugin, you will not be able to check units when using mouse controls.
+   That means any games using this are basically unplayable with solo-mouse, so if you use this, you'll want to disable your game's mouse controls.
+   I do intend to fix this problem.
+
+   Update history:
+   5/18/2020: fix so that Cancel still cancels switching units during battle prep
+   6/9/2020: fix so that Cancel still shows enemy range during battle prep. It was 1 missing line duuuude
 */
 
 (function () {
@@ -26,5 +35,19 @@
     if (!this._isMarkingDisabled && InputControl.getInputType() === InputType.NONE) {
       MapLayer.getMarkingPanel().startMarkingPanel();
     }
+    return MapEditResult.MAPCHIPCANCEL;
   }
+
+  // pressing Cancel still cancels when switching units
+  var alias1 = SetupEdit._movePosChangeMode;
+  SetupEdit._movePosChangeMode = function(){
+    if(InputControl.isCancelState()){
+      this._targetObj = null;
+      this._playCancelSound();
+			this._mapEdit.disableMarking(true);
+			this.changeCycleMode(SetupEditMode.TOP);
+    }
+    return alias1.call(this);
+  }
+
 })();
